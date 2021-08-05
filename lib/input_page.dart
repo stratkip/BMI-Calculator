87eb99649f94ece1efd17bf6472d464f2b840afc
stratkip <stratkip@yandex.ru>
2021-08-05 12:39:41 +0300

    Update input_page.dart

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -81,7 +81,6 @@ class _InputPageState extends State<InputPage> {
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.baseline,
                     children: [
                       Text(
                         height.toString(),
